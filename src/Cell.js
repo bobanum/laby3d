a8fb76f034b43defb076569309d7a2501c8263bb
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-04-07 07:18:17 -0400

    essai

--- src/Cell.js
+++ src/Cell.js
@@ -1,8 +1,10 @@
-export default class Cell {
+import Point from "./Point.js";
+export default class Cell extends Point {
     constructor(x, y, z) {
-        this.directions = [0,1,2,3,4,5];
+        super(x, y, z);
+        this.directions = [0, 1, 2, 3, 4, 5];
         this.shuffle(this.directions);
-        this.walls = [1,1,1,1,1,1];
+        this.walls = [1, 1, 1, 1, 1, 1];
     }
     shuffle(array) {
         for (let i = array.length; i > 0; i -= 1) {
@@ -15,5 +17,56 @@ export default class Cell {
     }
     toString() {
         return this.walls.join("");
+    }
+    get dom() {
+        var folder = "images/petit";
+        var dx = 64;
+        var dy = 32;
+        // var dd = 4;
+        var dd = 2;
+        // var folder = "images/minuscule";
+        // var dx = 32;
+        // var dy = 16;
+        if (!this._dom) {
+            var span = document.createElement("span");
+            var img = span.appendChild(document.createElement("img"));
+            img.setAttribute("src", folder+"/floor_E.png");
+            img.style.position = "absolute";
+            if (this.walls[0]) {
+                var img = span.appendChild(document.createElement("img"));
+                img.setAttribute("src", folder+"/wall_E.png");
+                img.style.position = "absolute";
+                img.style.marginTop = (-7*dd)+"px"
+                img.style.marginLeft = (4*dd)+"px";
+            }
+            if (this.walls[4]) {
+                var img = span.appendChild(document.createElement("img"));
+                img.setAttribute("src", folder+"/wall_N.png");
+                img.style.position = "absolute";
+                img.style.marginTop = (-7*dd)+"px"
+                img.style.marginLeft = (-4*dd)+"px";
+            }
+            if (this.walls[1]) {
+                var img = span.appendChild(document.createElement("img"));
+                img.setAttribute("src", folder+"/wall_S.png");
+                img.style.position = "absolute";
+                img.style.marginTop = (-3*dd)+"px"
+                img.style.marginLeft = (4*dd)+"px";
+            }
+            if (this.walls[3]) {
+                var img = span.appendChild(document.createElement("img"));
+                img.setAttribute("src", folder+"/wall_W.png");
+                img.style.position = "absolute";
+                img.style.marginTop = (-3*dd)+"px"
+                img.style.marginLeft = (-4*dd)+"px";
+            }
+            span.style.position = "absolute";
+            span.style.top = (-this.x + this.y) * dy + "px";
+            span.style.left = (this.x + this.y) * dx + "px";
+            span.style.zIndex = -this.x;
+            this._dom = span;
+            span.obj = this;
+        }
+        return this._dom
     }
 }
--- src/Laby.js
+++ src/Laby.js
@@ -19,7 +19,7 @@ export default class Laby {
             for (let y = 0; y < this.depth; y += 1) {
                 this.cells[z][y] = [];
                 for (let x = 0; x < this.width; x += 1) {
-                    this.cells[z][y][x] = new Cell()
+                    this.cells[z][y][x] = new Cell(x, y, z);
                 }
             }
         }
@@ -44,19 +44,63 @@ export default class Laby {
                 cursor.dir = cell.directions.shift();
                 cursor.forward();
                 let newCell = cursor.steps[0];
-                // if (!newCell) {
-                //     console.log(this);
-                // }
-                if (!newCell || newCell.isOpen) {
+                if (!newCell) {
+                    cursor.backward();
+                } else if (newCell.isOpen) {
+                    newCell.directions = newCell.directions.filter(dir => dir !== (cursor.dir+3)%6);
                     cursor.backward();
                 } else {
                     newCell.directions = newCell.directions.filter(dir => dir !== (cursor.dir+3)%6);
                     cell.walls[cursor.dir] = 0;
                     cursor.steps[0].walls[(cursor.dir+3)%6] = 0;
+                    cell = newCell;
                 }
             }
             cursor.steps.shift();
         }
+    }
+    ascii(floor) {
+        // w0=S; w1=E; w2=U; w3=N; w4=W; w5=D
+        //          "0123456789abcdef"
+        var corners="0000║╠0╦╝╣╩╬";
+        var choix = ["╔═╗","║ ║","╚═╝"];
+        floor=this.cells[floor];
+        var result = []
+        for (let y = 0; y < this.depth; y += 1) {
+            result[y*3] = [];
+            result[y*3+1] = [];
+            result[y*3+2] = [];
+            for (let x = 0; x < this.width; x += 1) {
+                let cell = floor[y][x];
+                console.log(cell.walls);
+                result[y*3][x*3] = "+";
+                result[y*3][x*3+2] = "+";
+                result[y*3+2][x*3] = "+";
+                result[y*3+2][x*3+2] = "+";
+                result[y*3][x*3+1] = floor[y][x].walls[0] == 1 ? "-" : " ";
+                result[y*3+1][x*3] = floor[y][x].walls[1] == 1 ? "|" : " ";
+                result[y*3][x*3+2] = floor[y][x].walls[3] == 1 ? "-" : " ";
+                result[y*3+2][x*3] = floor[y][x].walls[4] == 1 ? "|" : " ";
+                result[y*3+1][x*3+1] = "x";
+            }
+            result[y*3] = result[y*3].join("");
+            result[y*3+1] = result[y*3+1].join("");
+            result[y*3+2] = result[y*3+2].join("");
+        }
+        result = result.join("\r\n");
+        return result;
+    }
+    display(element, floor=0) {
+        floor=this.cells[floor];
+        element = document.body.querySelector(element);
+        for (let y = 0; y < this.depth; y += 1) {
+            let row = element.appendChild(document.createElement("div"));
+            for (let x = 0; x < this.width; x += 1) {
+                row.appendChild(floor[y][x].dom);
+                console.log(floor[y][x]);
+            }
+        }
+
     }
     toString() {
         return this.cells;
